5f6583081158bf32e5e23b954a275bcaa0bd8860
OliverDanger <od.oliverdixon@gmail.com>
2023-05-07 06:40:50 +0000

    setup basic auth and update env.example

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ProductsController < ApplicationController
 
+  http_basic_authenticate_with name: "oliver", password: "danger"
+
   def index
     @products = Product.order(id: :desc).all
   end

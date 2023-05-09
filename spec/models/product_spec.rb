f28cbd12d6cfea4ff3092ed09e1b947999f8c64c
OliverDanger <od.oliverdixon@gmail.com>
2023-05-09 21:23:38 +0000

    rspec check product name

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,32 @@
 require 'rails_helper'
+require 'products_controller'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+
+
+    before do
+      @product = Product.new(
+        :name => "Plant One",
+        :description => "It's green and alive.",
+        :image => "https://www.houseplantsexpert.com/wp-content/uploads/2022/09/syngonium_podophyllum.jpg",
+        :price_cents => 99,
+        :quantity => 1234
+      )
+      # puts @product.inspect
+    end
     # validation tests/examples here
+    describe "#add" do
+      it "works!" do
+        expect(1 + 1).to eql(2)
+      end
+    end
+
+    describe "#name is present" do
+      it "checks for name" do
+        expect( @product.name ).to eq("Plant One")
+      end
+    end
+
   end
 end

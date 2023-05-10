6410e4603df1250e6d4b33fec64e42c8112388a6
OliverDanger <od.oliverdixon@gmail.com>
2023-05-10 01:24:04 +0000

    tests user model creates valid user, and removes logs from product_spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -25,7 +25,6 @@ RSpec.describe Product, type: :model do
       it "is not valid without a name" do
         @product.name = nil
         @product.save
-        puts @product.errors.full_messages.inspect
         expect( @product ).to_not be_valid
         expect( @product.errors.full_messages ).to eq(["Name can't be blank"])
       end
@@ -33,7 +32,6 @@ RSpec.describe Product, type: :model do
       it "is not valid without a price" do
         @product.price_cents = nil
         @product.save
-        puts @product.errors.full_messages.inspect
         expect( @product ).to_not be_valid
         expect( @product.errors.full_messages ).to eq(["Price cents is not a number", "Price is not a number", "Price can't be blank"])
       end
@@ -41,7 +39,6 @@ RSpec.describe Product, type: :model do
       it "is not valid without a quantity" do
         @product.quantity = nil
         @product.save
-        puts @product.errors.full_messages.inspect
         expect( @product ).to_not be_valid
         expect( @product.errors.full_messages ).to eq(["Quantity can't be blank"])
       end
@@ -49,7 +46,6 @@ RSpec.describe Product, type: :model do
       it "is not valid without a category" do
         @product.category_id = nil
         @product.save
-        puts @product.errors.full_messages.inspect
         expect( @product ).to_not be_valid
         expect( @product.errors.full_messages ).to eq(["Category must exist", "Category can't be blank"])
       end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+
+    before do
+      @user = User.new(
+        email: "abc@def.ghi",
+        firstname: "Alphabet",
+        lastname: "Jones",
+        password_digest: "1234567890"
+      )
+    end
+
+
+    describe "#user" do
+      it "is valid with valid attributes" do
+        expect( @user ).to be_valid
+      end
+
+      # to be converted:
+      
+      # it "is not valid without a name" do
+      #   @product.name = nil
+      #   @product.save
+      #   puts @product.errors.full_messages.inspect
+      #   expect( @product ).to_not be_valid
+      #   expect( @product.errors.full_messages ).to eq(["Name can't be blank"])
+      # end
+  
+      # it "is not valid without a price" do
+      #   @product.price_cents = nil
+      #   @product.save
+      #   puts @product.errors.full_messages.inspect
+      #   expect( @product ).to_not be_valid
+      #   expect( @product.errors.full_messages ).to eq(["Price cents is not a number", "Price is not a number", "Price can't be blank"])
+      # end
+    
+      # it "is not valid without a quantity" do
+      #   @product.quantity = nil
+      #   @product.save
+      #   puts @product.errors.full_messages.inspect
+      #   expect( @product ).to_not be_valid
+      #   expect( @product.errors.full_messages ).to eq(["Quantity can't be blank"])
+      # end
+  
+      # it "is not valid without a category" do
+      #   @product.category_id = nil
+      #   @product.save
+      #   puts @product.errors.full_messages.inspect
+      #   expect( @product ).to_not be_valid
+      #   expect( @product.errors.full_messages ).to eq(["Category must exist", "Category can't be blank"])
+      # end
+    end
+
+  end
 end

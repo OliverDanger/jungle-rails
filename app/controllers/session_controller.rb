e2483e9b88d4ebf578bfb15c5613afcaad0d59c7
OliverDanger <od.oliverdixon@gmail.com>
2023-05-10 02:35:12 +0000

    move authenticate_with_credentials method to User model

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -2,20 +2,10 @@ class SessionController < ApplicationController
   def new
   end
 
-  # verify user on login
-  def authenticate_with_credentials( email, password)
-    email.downcase
-    user = User.find_by_email(email)
-    if user
-      return user.authenticate(password)
-    end
-    return nil
-  end
-
   def create
     user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct.
-    if authenticate_with_credentials( params[:email], params[:password] )
+    if User.authenticate_with_credentials( params[:email], params[:password] )
       # Save the user id inside the browser cookie.
       session[:user_id] = user.id
       redirect_to '/'
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ApplicationRecord
   validates :firstname, presence: true
   validates :lastname, presence: true
   validates :password, length: { minimum: 7 }
+
+    # verify user on login
+  def authenticate_with_credentials( email, password)
+    email.downcase
+    user = User.find_by_email(email)
+    if user
+      return user.authenticate(password)
+    end
+    return nil
+  end
 end

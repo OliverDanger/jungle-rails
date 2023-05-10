17053fbb22f275de6967a4db6fbae7066386bd53
OliverDanger <od.oliverdixon@gmail.com>
2023-05-10 22:09:49 +0000

    cypress test Cart total increments up one when add product is clicked

--- cypress/integration/add_to_cart.spec.js
+++ cypress/integration/add_to_cart.spec.js
@@ -13,6 +13,12 @@ describe('# Add to Cart ', () => {
   });
 
   it("There is an 'Add' button for the first product", () => {
-    cy.get(':nth-child(1) > .product-buttons > :nth-child(2) > .button_to > .btn').contains('Add').should('be.visible')
+    cy.get(':nth-child(1) > .product-buttons > :nth-child(2)').contains('Add').should('be.visible')
   })
+
+  it("Cart total increments up one when add product is clicked", () => {
+    cy.get('.end-0 > .nav-link').contains('My Cart (0)').should('be.visible')
+    cy.get(':nth-child(1) > .product-buttons > :nth-child(2)').contains('Add').click({force: true})
+    cy.get('.end-0 > .nav-link').contains('My Cart (1)').should('be.visible')
+  });
 })
